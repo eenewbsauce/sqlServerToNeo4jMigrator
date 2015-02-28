c0bb77b2d8d301c56ad0452a159df63c5467616c
mguinny <mguignard@gmail.com>
2015-02-28 14:30:46 -0500

    added days of week node

--- 1aMigrator/Program.cs
+++ 1aMigrator/Program.cs
@@ -13,68 +13,61 @@ namespace _1aMigrator
     {
         static void Main(string[] args)
         {
-            var legitBusinesses = new List<UserProfile>();
-
             using (var db = new onea { })
             {
                 var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
                 client.Connect();
 
-                var businesses = db.UserProfiles.ToList();
+                //CreateBusinessNodes(db, client);
+                CreateDaysOfWeekNodes(client);
+            }
+        }
 
-                using (var conn = new SqlConnection("Server=RT-PC\\SQL2012;Database=PourtraitBeta;Trusted_Connection=True;"))
+        private static void CreateDaysOfWeekNodes(GraphClient client)
+        {
+            var days = new string[]{"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"};
+            foreach (var day in days) 
+            {
+                client.Cypher
+                .Create("(user:day {day})")
+                .WithParam("day", new { day = day })
+                .ExecuteWithoutResults();
+            }            
+        }
+
+        private static void CreateBusinessNodes(onea db, GraphClient client)
+        {
+            var businesses = db.UserProfiles.ToList();
+
+            using (var conn = new SqlConnection("Server=RT-PC\\SQL2012;Database=PourtraitBeta;Trusted_Connection=True;"))
+            {
+                conn.Open();
+
+                foreach (var business in businesses)
                 {
-                    conn.Open();
-
-                    foreach (var business in businesses)
+                    using (var cmd = new SqlCommand("IsUseraBusiness", conn))
                     {
-                        using (var cmd = new SqlCommand("IsUseraBusiness", conn))
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.Add("@userId", SqlDbType.Int).Value = business.UserId;
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
                         {
-                            cmd.CommandType = CommandType.StoredProcedure;
+                            var x = reader[0];
 
-                            cmd.Parameters.Add("@userId", SqlDbType.Int).Value = business.UserId;
-                            SqlDataReader reader = cmd.ExecuteReader();
-
-                            while (reader.Read())
+                            if (x != null)
                             {
-                                var x = reader[0];
-
-                                if (x != null)
-                                {
-                                    legitBusinesses.Add(business);
-                                    client.Cypher
-                                        .Create("(user:business {biz})")
-                                        .WithParam("biz", new { name = business.Name })
-                                        .ExecuteWithoutResults();
-                                }
+                                client.Cypher
+                                    .Create("(user:business {biz})")
+                                    .WithParam("biz", new { name = business.Name })
+                                    .ExecuteWithoutResults();
                             }
-
-                            reader.Close();
                         }
-                    }                    
+
+                        reader.Close();
+                    }
                 }
-
-
-                //foreach (var biz in businesses)
-                //{
-                //    try
-                //    {
-                //        var business = biz;
-
-                //        if (business == null)
-                //            continue;
-                //        var businessName = business;
-
-                //        client.Cypher
-                //            .Create("(user:business {biz})")
-                //            .WithParam("biz", new { name = businessName })
-                //            .ExecuteWithoutResults();
-                //    }
-                //    catch (Exception e)
-                //    {
-                //        continue;
-                //    }
-                //}
             }
         }
     }
